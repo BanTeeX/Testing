407aee91851c6596835467225031e9e7fc9ff747
Filip <filipbanaszak333@gmail.com>
2021-01-21 17:37:49 +0100

    Update5

--- TestCpp/Test.cpp
+++ TestCpp/Test.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-struct samochod
+int main()
 {
-	string marka;
-	string model;
-	int rok = 0;
+	setlocale(LC_CTYPE, "Polish");
+	string polskie = "¹êæ¿Ÿó";
+	cout << polskie << endl;
 
-	samochod(string marka, string model, int rok) : marka(marka), model(model), rok(rok) { }
-
-	void Print()
-	{
-		cout << "Marka: " << marka << ", model: " << model << ", rok: " << rok << ";" << endl;
-	}
-};
-
-//int main()
-//{
-//	samochod moj("Audi", "A7", 2015);
-//	samochod anny("BMW", "M5", 2017);
-//
-//	samochod moj = samochod("Audi", "A7", 2015);
-//	samochod anny = samochod("BMW", "M5", 2017);
-//
-//	moj.Print();
-//	anny.Print();
-//}
+	fstream file;
+	file.open("plik.txt", ios::in);
+	string polskie2;
+	getline(file, polskie2);
+	cout << polskie2 << endl;
+	file.close();
+}
